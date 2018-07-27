fcf2c37c288a4e7e3d4daeff42a306ea22cfff70
alexandre.depembroke <alexandre.depembroke@businessdecision.com>
2018-07-27 09:57:17 +0200

    test routes

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,4 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('contact', 'HomeController@contact');
+Route::post('contact', 'HomeController@contact');
